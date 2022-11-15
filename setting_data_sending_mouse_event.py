0aa2ec48fd9e576dfddef81568e8e7202802c03e
Vikkysoft <bolvic28@gmail.com>
2022-11-15 08:07:32 +0000

    initial commit

--- setting_data_sending_mouse_event.py
+++ setting_data_sending_mouse_event.py
@@ -17,15 +17,15 @@ def main():
     send_keys_to_element(driver.find_element(By.NAME, "lastname"), "Victoria Bolupe")
     send_keys_to_element(driver.find_element(By.NAME, "email"), "bolvic28@gmail.com")
     send_keys_to_element(driver.find_element(By.NAME, "phone"), Keys.CONTROL, "V")
-    send_keys_to_element(driver.find_element(By.NAME, "message"), "I'm just practising our Selenium/python automation class")
+    send_keys_to_element(driver.find_element(By.NAME, "message"),
+                         "I'm just practising our Selenium/python automation class")
 
     form = driver.find_element(By.TAG_NAME, "form")
-    #submit_button = form.find_element(By.TAG_NAME, "button")
-    #submit_button.click()
+    # submit_button = form.find_element(By.TAG_NAME, "button")
+    # submit_button.click()
     form.find_element(By.XPATH, '//*[@id="__next"]/main/section[1]/div/div[1]/div[2]/form/div[5]/button').click()
     form.find_element(By.TAG_NAME, "button").click()
     time.sleep(15)
-
 
 
 if __name__ == '__main__':
